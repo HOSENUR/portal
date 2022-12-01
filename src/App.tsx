291f04cc75a63d817fc2f6c943033fb8eaea934d
Hosenur Rahaman <nur08439@gmail.com>
2022-12-01 22:56:37 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Landing from './pages/Landing'
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </Router>
+  )
+}
